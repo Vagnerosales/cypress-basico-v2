c4a738de98eef60144abffdc1247c6f9ad738ec3
Vagner <vagnerosales@gmail.com>
2024-02-29 17:13:52 -0300

    Create cypress project

--- cypress/e2e/CAC-TAT.spec.cy.js
+++ cypress/e2e/CAC-TAT.spec.cy.js
@@ -237,7 +237,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
             expect(body).to.include('CAC TAT')
           })
         })
-      it.only('encontra o gato escondido', function(){
+      it('encontra o gato escondido', function(){
         cy.get('#cat')
           .invoke('show')
           .should('be.visible')
